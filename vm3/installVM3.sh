2cb4cb40d827f61bcdd79170f6efa3f43f3c4007
Ubuntu <guestExercise@exercise-cef-3.yb25l5mvbujevinws3s1wucgtf.bx.internal.cloudapp.net>
2019-11-19 11:14:21 +0000

    handle vm3

--- vm3/installVM3.sh
+++ vm3/installVM3.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
-sudo cp security-config-omsagent.conf /etc/rsyslog.d/security-config-omsagent.conf
+
 
 sudo cp rsyslog.conf /etc/rsyslog.conf
-
 
 sudo /opt/microsoft/omsagent/bin/service_control restart  aef4d9e8-72bd-4a43-a0c1-0b7ec2285952
 
 sudo service rsyslog restart
-
-
-
-
 
 
